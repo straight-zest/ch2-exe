c48a687fd4645fc55bef74e721b5fe46e61d6002
gabriel.carlile <gabriel.carlile@CLC-LAB209-01.TCSD.tooelesd.org>
2018-09-06 14:06:47 -0600

    ch2 exe

---
+++ chapter 2exe/src/gabriel_carlile/ch2exe/Ch2_exe.java
@@ -0,0 +1,20 @@
+package gabriel_carlile.ch2exe;
+/*
+ * Gabriel Carlile
+ * 9-18
+ * ch2 exe
+ */
+public class Ch2_exe {
+
+	public static void main(String[] args) {
+		// display some text
+		System.out.println("Welcome to Java");
+		System.out.println("you belong to the system");
+		System.out.println("join us");
+		System.out.println("or perish");
+		System.out.println(2.68+24);
+		
+
+	}
+
+}
